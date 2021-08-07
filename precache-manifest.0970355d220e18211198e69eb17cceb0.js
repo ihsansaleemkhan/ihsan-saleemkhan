6c10a8b7274c6c4aafcad7a75054aeda190824e4
ihsansaleemkhan <saleemkhan.ihsan@gmail.com>
2021-08-07 11:12:08 +0300

    Updates

---
+++ precache-manifest.0970355d220e18211198e69eb17cceb0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7e5f31592ef9737f4e3bca1a7096ac",
+    "url": "/ihsan-saleemkhan/index.html"
+  },
+  {
+    "revision": "5d986d57b2eb01676a37",
+    "url": "/ihsan-saleemkhan/static/css/main.c4fdaf3c.chunk.css"
+  },
+  {
+    "revision": "1207cdc7565f8cb0cbd6",
+    "url": "/ihsan-saleemkhan/static/js/2.456d47ea.chunk.js"
+  },
+  {
+    "revision": "e4e2e5c39889c7bbeb7a279dcd0ea8bc",
+    "url": "/ihsan-saleemkhan/static/js/2.456d47ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b746bb25ee1e3f1090",
+    "url": "/ihsan-saleemkhan/static/js/3.ddf199b8.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/ihsan-saleemkhan/static/js/3.ddf199b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e492b33862e5886958",
+    "url": "/ihsan-saleemkhan/static/js/4.ed1dfddb.chunk.js"
+  },
+  {
+    "revision": "809abad1ab3333af372fdc0cd7f2829d",
+    "url": "/ihsan-saleemkhan/static/js/4.ed1dfddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb470b7d74188e59d46",
+    "url": "/ihsan-saleemkhan/static/js/5.b4bc2922.chunk.js"
+  },
+  {
+    "revision": "7303fb104718deedc447d0f351b45d55",
+    "url": "/ihsan-saleemkhan/static/js/5.b4bc2922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d986d57b2eb01676a37",
+    "url": "/ihsan-saleemkhan/static/js/main.f2ab1f5c.chunk.js"
+  },
+  {
+    "revision": "75f17e50d43ca726e7e1",
+    "url": "/ihsan-saleemkhan/static/js/runtime-main.91cdaa44.js"
+  },
+  {
+    "revision": "1e47ff18f781ec213a746daef109352d",
+    "url": "/ihsan-saleemkhan/static/media/DTS.1e47ff18.png"
+  },
+  {
+    "revision": "52c7fd36302c1600548572cd6ca10914",
+    "url": "/ihsan-saleemkhan/static/media/admin.52c7fd36.png"
+  },
+  {
+    "revision": "d8df0923ccf5887393ee35c4c1aef9fc",
+    "url": "/ihsan-saleemkhan/static/media/asset-management.d8df0923.png"
+  },
+  {
+    "revision": "15b23a4f8dad6d0a89f091d01b419bf4",
+    "url": "/ihsan-saleemkhan/static/media/avatar.15b23a4f.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/ihsan-saleemkhan/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "0a6f116f3c4469c6a832543d85131fa9",
+    "url": "/ihsan-saleemkhan/static/media/ihsan-saleemkhan1.0a6f116f.jpeg"
+  },
+  {
+    "revision": "cf075920efd9c8b6ee994df3c81d6fb5",
+    "url": "/ihsan-saleemkhan/static/media/ihsan-saleemkhan2.cf075920.jpeg"
+  },
+  {
+    "revision": "946d996eb1d6ab58e5c7a72a098cd870",
+    "url": "/ihsan-saleemkhan/static/media/ihsan-saleemkhan3.946d996e.jpeg"
+  }
+]);
